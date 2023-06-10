0adfba39c25dc96f520cdae95c61a0ef9270ce81
Corey Sawin <sawincp@gmail.com>
2023-06-10 13:57:27 -0400

    update seed file with faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
-puts "🌱 Seeding spices..."
+require 'faker'
 
-# Seed your database here
+puts "🌱 Seeding..."
+
+# run a loop 50 times
+50.times do
+    # create a game with random data
+    game = Game.create(
+      title: Faker::Game.title,
+      genre: Faker::Game.genre,
+      platform: Faker::Game.platform,
+      price: rand(0..60) # random number between 0 and 60
+    )
+  
+    # create between 1 and 5 reviews for each game
+    rand(1..5).times do
+      Review.create(
+        score: rand(1..10),
+        comment: Faker::Lorem.sentence,
+        game_id: game.id # use the ID (primary key) of the game as the foreign key
+      )
+    end
+  end
 
 puts "✅ Done seeding!"
